b8168dac841424406c21970ff0f47e1ce4d21897
SHAKTHI84 <113224281+SHAKTHI84@users.noreply.github.com>
2022-09-10 09:15:27 +0530

    Create rectangle.cpp

---
+++ rectangle.cpp
@@ -0,0 +1,17 @@
+#include <algorithm>
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int main() {
+	int l,b,a,p;
+	cin>>l>>b;
+	a=l*b;
+	
+	p=2*(l+b);
+	cout<<a<<"\n"<<p;
+	// Your code goes here;
+
+	return 0;
+}
